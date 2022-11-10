e20f66e92b481f7a96f29094197eba0df26ae8a5
sunny liu <372930@gmail.com>
2022-11-10 11:11:10 +0900

    not finish the show.html.erb part

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :restaurants, only: [:index, :create, :new, :show] do
+    resources :reviews, only: [:create, :new]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
